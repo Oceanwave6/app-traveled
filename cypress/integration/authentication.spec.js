080e3d65a267242fe8bb99627c3a3674c8c05405
maxime blanc <maximeblanc.dev@gmail.com>
2018-04-19 17:56:48 +0200

    init tests

---
+++ cypress/integration/authentication.spec.js
@@ -0,0 +1,30 @@
+describe('=== Authentication ===', () => {
+  beforeEach(() => {
+    cy.visit('/login')
+  })
+
+  it('should display the login page', () => {
+    cy.get(':nth-child(1) > .MuiInput-root-8 > .MuiInput-input-16').as('emailInput')
+    cy.get(':nth-child(2) > .MuiInput-root-8 > .MuiInput-input-16').as('passwordInput')
+    cy.get('.MuiButtonBase-root-36').as('loginButton')
+
+    cy.fixture('authentication.json').then(authJson => {
+      cy.get('@emailInput').type(authJson.email)
+      cy.get('@passwordInput').type(authJson.goodPassword)
+      cy.get('@loginButton').click()
+    })
+  })
+
+  it.only('should register a new user', () => {
+    cy.get(':nth-child(2) > a').as('registerLink')
+    cy.get('@registerLink').click()
+
+    cy.get(':nth-child(2) > .MuiInput-root-8 > .MuiInput-input-16').as('emailInput')
+    cy.get(':nth-child(4) > .MuiInput-root-8 > .MuiInput-input-16').as('passwordInput')
+
+    cy.fixture('authentication.json').then(authJson => {
+      cy.get('@emailInput').type(authJson.email)
+      cy.get('@passwordInput').type(authJson.goodPassword)
+    })
+  })
+})
