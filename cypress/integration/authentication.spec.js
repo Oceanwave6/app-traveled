273a0e18de39477e1c3e2651ae528a8e3d47da8f
maxime blanc <maximeblanc.dev@gmail.com>
2018-04-20 09:15:20 +0200

    wip

--- cypress/integration/authentication.spec.js
+++ cypress/integration/authentication.spec.js
@@ -7,9 +7,9 @@ describe('=== Authentication ===', () => {
   })
 
   it.only('should display the login page', () => {
-    cy.get(':nth-child(1) > .MuiInput-root-8 > .MuiInput-input-16').as('emailInput')
-    cy.get(':nth-child(2) > .MuiInput-root-8 > .MuiInput-input-16').as('passwordInput')
-    cy.get('.MuiButtonBase-root-36').as('loginButton')
+    cy.get(':nth-child(1) > .MuiInput-root-6 > .MuiInput-input-14').as('emailInput')
+    cy.get(':nth-child(2) > .MuiInput-root-6 > .MuiInput-input-14').as('passwordInput')
+    cy.get('.MuiButtonBase-root-34').as('loginButton')
 
     cy.fixture('authentication.json').then(authJson => {
       cy.get('@emailInput').type(authJson.email)
