3f17ae6bded32563883ebaee7812ab033167e426
maxime blanc <maximeblanc.dev@gmail.com>
2018-04-24 11:12:57 +0200

    add stepper

--- src/pages/Travel/TravelCreationPage.js
+++ src/pages/Travel/TravelCreationPage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { observer, inject } from 'mobx-react'
+import Stepper, { Step, StepButton } from 'material-ui/Stepper'
 
 import TravelCreationPageOne from './TravelCreationPageOne'
 import TravelCreationPageTwo from './TravelCreationPageTwo'
@@ -13,22 +14,34 @@ import Navbar from '../../components/Navbar'
 class TravelCreationPage extends Component {
   state = {
     participants: [this.props.userStore.user.uid],
-    step: 1
+    activeStep: 0,
+    completed: new Set(),
+    skipped: new Set()
   }
 
   handleInputsChange = ({ target: { name, value } }) => {
     this.props.travelStore.updateTravelCreation(name, value)
   }
 
+  handleStep = step => () => {
+    this.setState({
+      activeStep: step
+    })
+  }
+
+  isStepComplete (step) {
+    return this.state.completed.has(step)
+  }
+
   renderSwitch = () => {
-    switch (this.state.step) {
-      case 1:
+    switch (this.state.activeStep) {
+      case 0:
         return <TravelCreationPageOne handleInputsChange={this.handleInputsChange} />
 
-      case 2:
+      case 1:
         return <TravelCreationPageTwo handleInputsChange={this.handleInputsChange} />
 
-      case 3:
+      case 2:
         return <TravelCreationPageThree handleInputsChange={this.handleInputsChange} />
 
       default:
@@ -41,6 +54,21 @@ class TravelCreationPage extends Component {
         <Header />
         <Navbar />
         {this.renderSwitch()}
+
+        <Stepper alternativeLabel nonLinear activeStep={this.state.activeStep}>
+          {['Select campaign settings', 'Create an ad group', 'Create an ad'].map(
+            (label, index) => {
+              return (
+                <Step key={label}>
+                  <StepButton
+                    onClick={this.handleStep(index)}
+                    completed={this.isStepComplete(index)}
+                  />
+                </Step>
+              )
+            }
+          )}
+        </Stepper>
       </div>
     )
   }
